34228d0865f1cace53ba25c121b7045173ff5c5e
leandrolog <leandro.oscargn@gmail.com>
2023-09-09 14:32:18 -0300

    segund commit

---
+++ src/pages/match/Match.js
@@ -0,0 +1,16 @@
+import './match.css'
+
+
+function Match() {
+
+    return (
+        <div className="content" >
+            <div>
+                <h1>Match page</h1>
+            </div>
+        </div>
+    )
+
+}
+
+export default Match
---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,15 @@
+
+
+
+function Profile (){
+
+    return(
+        <div className="content" >
+            <div>
+                <h1>Profile page</h1>
+            </div>
+        </div>
+    )
+
+}
+export default Profile
