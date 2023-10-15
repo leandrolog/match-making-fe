20b02f9b5490ce5a5ebba986d98af57d901362ac
leandrolog <leandro.oscargn@gmail.com>
2023-10-14 22:10:33 -0300

    salvando aterações

--- src/pages/match/Match.js
+++ src/pages/match/Match.js
@@ -1,13 +1,20 @@
 import './match.css'
+import {Button} from 'react-bootstrap';
+import CardBox from "../../components/card/CardBox";
 
 
 function Match() {
 
     return (
-        <div className="content" >
-            <div>
+        <div className="content">
+            <Button
+                className="match-button"
+                >Criar
+            </Button>
+            <div className="match-content">
                 <h1>Match page</h1>
             </div>
+            <CardBox/>
         </div>
     )
 
